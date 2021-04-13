78d9abd5d163495877ccfe4866115f336b665828
FabrizioAtiquipa <fabrizio.atiquipa@tecsup.edu.pe>
2021-04-12 23:17:43 -0500

    1.1-Votaciones-Refatorizando Statistics

--- Votaciones/src/index.js
+++ Votaciones/src/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import Statistics from './Components/Statistics'
+import Statistics from "./Components/Statistics";
+import Button from "./Components/Button";
 
 const App = () => {
   // save clicks of each button to its own state
@@ -8,32 +9,29 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const [promedio, setPromedio] = useState(0)
+  const [promedio, setPromedio] = useState(0);
 
   const BUENO = () => {
     setGood(good + 1);
-    setPromedio(promedio+1)
+    setPromedio(promedio + 1);
   };
   const NEUTRAL = () => {
     setNeutral(neutral + 1);
-    setPromedio(promedio+0)
+    setPromedio(promedio + 0);
   };
   const MALO = () => {
     setBad(bad + 1);
-    setPromedio(promedio-1)
+    setPromedio(promedio - 1);
   };
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={BUENO}>good</button>
-      <button onClick={NEUTRAL}>neutral</button>
-      <button onClick={MALO}>bad</button>
 
-      
+      <Button estado="good" boton={BUENO} />
+      <Button estado="neutral" boton={NEUTRAL} />
+      <Button estado="bad" boton={MALO} />
+
       <Statistics good={good} neutral={neutral} bad={bad} promedio={promedio} />
-      
-
     </div>
   );
 };
